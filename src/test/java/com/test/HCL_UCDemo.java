f9a6242ddeadbc9446a7b683e6c60c03c2a63159
Computer <rajkumar888@gmail.com>
2021-08-07 15:29:08 +0800

    implemented extent reports

--- src/test/java/com/test/HCL_UCDemo.java
+++ src/test/java/com/test/HCL_UCDemo.java
@@ -1,29 +1,57 @@
 package com.test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.pages.Branding;
 import com.utils.Drivers;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class HCL_UCDemo {
 
-    public String browser = "chrome";
     public WebDriver driver;
-    public static Logger log = Logger.getLogger(HCL_UCDemo.class);
+    public Logger log = Logger.getLogger(HCL_UCDemo.class);
+    public ExtentHtmlReporter htmlReporter;
+    public ExtentReports extent;
+    public ExtentTest test;
 
     @Parameters("browser")
-    @BeforeMethod
+    @BeforeTest
     public void setup(String browser) {
 
         PropertyConfigurator.configure("log4j.properties");
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/log/ExtentReport " + new SimpleDateFormat("dd_MMM_yyyy__HH_mm_ss").format(new Date()) + ".html");
+
+        //initialize ExtentReports and attach the HtmlReporter
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+
+        //To add system or environment info by using the setSystemInfo method.
+        extent.setSystemInfo("OS", "Windows");
+        extent.setSystemInfo("Browser", browser);
+
+        //configuration items to change the look and feel
+        //add content, manage tests etc
+        htmlReporter.config().setChartVisibilityOnOpen(true);
+        htmlReporter.config().setDocumentTitle("HCL Extent Report");
+        htmlReporter.config().setReportName("THCL est Report");
+        htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+        htmlReporter.config().setTheme(Theme.DARK);
+        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
 
         if (browser.equalsIgnoreCase("chrome")) {
             driver = new Drivers().launchDriver("chrome");
@@ -42,21 +70,37 @@ public class HCL_UCDemo {
 
     @Test
     public void UC_5() throws InterruptedException {
+
+        test = extent.createTest("UC_5", "UC 5 Started....");
+
         Branding branding = new Branding(driver);
         branding.navigateToBrandingPage();
 
-        String expectedTitle="Healthcare Branding Services to Differentiate You From the Competition";
-        String actualTitle=branding.getBrandingPageTitle();
-        Assert.assertEquals(expectedTitle,actualTitle,"Title didn't match");
+        String expectedTitle = "Healthcare Branding Services to Differentiate You From the Competition";
+        String actualTitle = branding.getBrandingPageTitle();
+        Assert.assertEquals(expectedTitle, actualTitle, "Title didn't match");
         Assert.assertTrue(branding.readAndStoreHeader1());
         Assert.assertTrue(branding.readAndStoreHeader2());
         Assert.assertTrue(branding.readAndStoreSubtext());
     }
 
     @AfterMethod
-    public void tearDown() {
-        log.info("Method to close the browser");
-        driver.quit();
+    public void getResult(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            test.log(Status.FAIL, MarkupHelper.createLabel(result.getName() + " FAILED ", ExtentColor.RED));
+            test.fail(result.getThrowable());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(Status.PASS, MarkupHelper.createLabel(result.getName() + " PASSED ", ExtentColor.GREEN));
+        } else {
+            test.log(Status.SKIP, MarkupHelper.createLabel(result.getName() + " SKIPPED ", ExtentColor.ORANGE));
+            test.skip(result.getThrowable());
+        }
     }
 
+    @AfterTest
+    public void tearDown() {
+        test.info("Method to close the browser");
+        driver.quit();
+        extent.flush();
+    }
 }
